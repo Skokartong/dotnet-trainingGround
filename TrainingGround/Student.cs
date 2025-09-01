95cbc2a5d60ebdcaa01457ca6fd1c156dbd9265f
Amanda Olving <amanda.olving@gmail.com>
2025-09-01 16:57:13 +0200

    Add 'Address' parameter in constructor for class 'Student' as well

--- TrainingGround/Student.cs
+++ TrainingGround/Student.cs
@@ -2,8 +2,8 @@ public class Student : Person
 {
     public string StudentId { get; private set; }
 
-    public Student(string name, int birthYear, double heightInMeters, string studentId)
-        : base(name, birthYear, heightInMeters)
+    public Student(string name, int birthYear, double heightInMeters, string studentId, Address? address = null)
+        : base(name, birthYear, heightInMeters, address)
     {
         StudentId = studentId;
     }
