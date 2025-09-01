2fe78f303901d3b88d742083692d57656ee8a246
Amanda Olving <amanda.olving@gmail.com>
2025-09-01 16:51:22 +0200

    Add one to one relationship between class 'Person' and 'Adress'

--- TrainingGround/Person.cs
+++ TrainingGround/Person.cs
@@ -3,20 +3,22 @@ public class Person
     public string? Name { get; private set; }
     public int BirthYear { get; private set; }
     public double HeightInMeters { get; private set; }
-
-    public Person(string name, int birthYear, double heightInMeters)
+    public Address? Address { get; set; }
+    public Person(string name, int birthYear, double heightInMeters, Address? address = null)
     {
         Name = name;
         BirthYear = birthYear;
         HeightInMeters = heightInMeters;
+        Address = address;
     }
 
     // Overlayering constructor to create person using age instead of birth year
-    public Person(string name, int age, double heightInMeters, bool isAge)
+    public Person(string name, int age, double heightInMeters, bool isAge, Address? address = null)
     {
         Name = name;
         BirthYear = DateTime.Now.Year - age;
         HeightInMeters = heightInMeters;
+        Address = address;
     }
 
     public int GetAge(int currentYear)
