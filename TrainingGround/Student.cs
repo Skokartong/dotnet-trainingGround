6486a64dcd903bc5a55b4517d261373055567c3a
Amanda Olving <amanda.olving@gmail.com>
2025-09-01 17:13:20 +0200

    Add interface 'IPrintable'

--- TrainingGround/Student.cs
+++ TrainingGround/Student.cs
@@ -1,4 +1,4 @@
-public class Student : Person
+public class Student : Person, IPrintable
 {
     public string StudentId { get; private set; }
 
@@ -6,5 +6,10 @@ public class Student : Person
         : base(name, birthYear, heightInMeters, address)
     {
         StudentId = studentId;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Student ID: {StudentId}, Name: {Name}, Birth Year: {BirthYear}, Height: {HeightInMeters}m, Address: {Address?.Street} {Address?.StreetNumber}, {Address?.ZipCode} {Address?.City}");
     }
 }
