51579f5856dbbc0917df16e36ff348cfe263c11f
Amanda Olving <amanda.olving@gmail.com>
2025-09-09 16:26:12 +0200

    Changed placement of test

--- TrainingGround.Tests/PersonTests.cs
+++ TrainingGround.Tests/PersonTests.cs
@@ -67,26 +67,4 @@ public class PersonTests
         // Assert
         Assert.Equal(expected, printedAddress);
     }
-
-    [Fact]
-    public void set_age_group_sets_correct_age_group()
-    {
-        // Arrange
-        var child = new Person("Anders", 2015, 1.2);
-        var teen = new Person("Veronica", 2008, 1.5);
-        var adult = new Person("Håkan", 1990, 2.1);
-        var senior = new Person("Viktoria", 1950, 1.6);
-
-        // Act
-        child.SetAgeGroup();
-        teen.SetAgeGroup();
-        adult.SetAgeGroup();
-        senior.SetAgeGroup();
-
-        // Assert
-        Assert.Equal(AgeGroup.Child, child.AgeGroup);
-        Assert.Equal(AgeGroup.Teen, teen.AgeGroup);
-        Assert.Equal(AgeGroup.Adult, adult.AgeGroup);
-        Assert.Equal(AgeGroup.Senior, senior.AgeGroup);
-    }
 }
